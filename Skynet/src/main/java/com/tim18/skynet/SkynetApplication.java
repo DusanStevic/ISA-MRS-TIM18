b886eca21f821ab5e0f41cb1692918b08a455620
Dusan Stevic <stevicdule@gmail.com>
2019-03-25 08:48:15 +0100

    inicijalno postavljanje projekta

---
+++ Skynet/src/main/java/com/tim18/skynet/SkynetApplication.java
@@ -0,0 +1,15 @@
+package com.tim18.skynet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkynetApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(SkynetApplication.class,args);
+
+	}
+
+}
