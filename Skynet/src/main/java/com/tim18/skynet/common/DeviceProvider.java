6e4724a4468e15dda056b62f070fa10d61e08147
Dusan Stevic <stevicdule@gmail.com>
2019-04-22 13:24:55 +0200

    dodavanje common paketa

---
+++ Skynet/src/main/java/com/tim18/skynet/common/DeviceProvider.java
@@ -0,0 +1,16 @@
+package com.tim18.skynet.common;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.mobile.device.Device;
+import org.springframework.mobile.device.DeviceUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeviceProvider {
+	
+	 public Device getCurrentDevice(HttpServletRequest request) {
+	        return DeviceUtils.getCurrentDevice(request);
+	    }
+
+}
