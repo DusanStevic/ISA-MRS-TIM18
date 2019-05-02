298ba1a8790ac6d3fbce769b6001869e173d3a10
Dusan Stevic <stevicdule@gmail.com>
2019-05-02 11:20:11 +0200

    unapredjenje interfesja AirlineAdministratora

--- Skynet/src/main/java/com/tim18/skynet/model/Airline.java
+++ Skynet/src/main/java/com/tim18/skynet/model/Airline.java
@@ -1,12 +1,18 @@
 package com.tim18.skynet.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column; 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Airline {
+public class Airline implements Serializable{
+	/**
+	 * 
+	 */
+	//private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue
 	private Long id;
@@ -74,4 +80,10 @@ public class Airline {
 	public void setImage(String image) {
 		this.image = image;
 	}
+
+
+
+	/*public static long getSerialversionuid() {
+		return serialVersionUID;
+	}*/
 }
