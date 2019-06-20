46ff978ab8800493dd2b02c4c1290bea36192af5
Dusan Stevic <stevicdule@gmail.com>
2019-06-20 22:40:19 +0200

    kacenje poslednje verzije sa gita 22:40

--- src/main/java/com/tim18/skynet/controller/FileController.java
+++ src/main/java/com/tim18/skynet/controller/FileController.java
@@ -2,23 +2,7 @@ package com.tim18.skynet.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.tim18.skynet.payload.UploadFileResponse;
-import com.tim18.skynet.service.FileStorageService;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -26,11 +10,11 @@ public class FileController {
 
     private static final Logger logger = LoggerFactory.getLogger(FileController.class);
 
-    @Autowired
-    private FileStorageService fileStorageService;
+   /* @Autowired
+    private FileStorageService fileStorageService;*/
    
     
-    @PostMapping(value="/api/uploadFile", produces = MediaType.APPLICATION_JSON_VALUE)
+    /*@PostMapping(value="/api/uploadFile", produces = MediaType.APPLICATION_JSON_VALUE)
     public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
     	String fileName = fileStorageService.storeFile(file);
         String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
@@ -41,17 +25,17 @@ public class FileController {
         System.out.println(fileDownloadUri);
         fileName = "addedImages/"+fileName;
         return new UploadFileResponse(fileName, fileDownloadUri,file.getContentType(), file.getSize());
-    }
+    }*/
 
-    @PostMapping("/api/uploadMultipleFiles")
+    /*@PostMapping("/api/uploadMultipleFiles")
     public List<UploadFileResponse> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
         return Arrays.asList(files)
                 .stream()
                 .map(file -> uploadFile(file))
                 .collect(Collectors.toList());
-    }
+    }*/
 
-    @GetMapping("/api/downloadFile/{fileName:.+}")
+    /*@GetMapping("/api/downloadFile/{fileName:.+}")
     public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
         // Load file as Resource
         Resource resource = fileStorageService.loadFileAsResource(fileName);
@@ -73,5 +57,5 @@ public class FileController {
                 .contentType(MediaType.parseMediaType(contentType))
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
-    }
+    }*/
 }
